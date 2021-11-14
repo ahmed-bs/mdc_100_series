073ded2b113607e6046eed5a2a4e5089661d26fb
ahmed-bs <medamed777@gmail.com>
2021-11-14 11:12:12 +0000

    //  Build a grid of cards (102)

--- lib/home.dart
+++ lib/home.dart
@@ -94,8 +94,33 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-        children: const <Widget>[Card()],
+        //  Build a grid of cards (102)
+
+        children: <Widget>[
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text('Title'),
+                      SizedBox(height: 8.0),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
       //Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
