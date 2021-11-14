51bc77b497dd19f39570c4ee94eddc97f88753a2
ahmed-bs <medamed777@gmail.com>
2021-11-14 09:32:33 +0000

    Add TextField widgets (101)

--- lib/login.dart
+++ lib/login.dart
@@ -39,6 +39,24 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             const SizedBox(height: 120.0),
+            // TODO: Add TextField widgets (101)
+// [Name]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+// spacer
+            const SizedBox(height: 12.0),
+// [Password]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
