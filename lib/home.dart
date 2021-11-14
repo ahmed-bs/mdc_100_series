a5331d2dcf6f7e8368c11edfc062db45598f9dec
ahmed-bs <medamed777@gmail.com>
2021-11-14 18:36:16 +0000

    8. Change the layout(103)

--- lib/home.dart
+++ lib/home.dart
@@ -13,10 +13,13 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 import 'model/products_repository.dart';
 import 'model/product.dart';
+import 'supplemental/asymmetric_view.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -90,84 +93,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return  Scaffold(
-      //  Add app bar (102)
+    return Scaffold(
       appBar: AppBar(
-        // Add buttons and title (102)
-          leading: IconButton(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: const Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
             icon: const Icon(
-              Icons.menu,
-              semanticLabel: 'menu',
+              Icons.search,
+              semanticLabel: 'search',
             ),
             onPressed: () {
-              print('Menu button');
+              print('Search button');
             },
           ),
-          title: const Text('SHRINE'),
-        // Add trailing buttons (102)
-        actions: <Widget>[
-      IconButton(
-      icon: const Icon(
-        Icons.search,
-        semanticLabel: 'search',
-      ),
-      onPressed: () {
-        print('Search button');
-      },
-    ),
-    IconButton(
-    icon: const Icon(
-    Icons.tune,
-    semanticLabel: 'filter',
-    ),
-    onPressed: () {
-    print('Filter button');
-    },
-    ),
-    ],
-      ),
-      // Add a grid view (102)
-    /*  body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        //  Build a grid of cards (102)
-
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const <Widget>[
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
+          IconButton(
+            icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter'
             ),
-          )
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
         ],
-      ),*/
-      // Add a grid view (102)
-      body: GridView.count(
-          crossAxisCount: 2,
-          padding: const EdgeInsets.all(16.0),
-          childAspectRatio: 8.0 / 9.0,
-          children: _buildGridCards(context) // Changed code
       ),
-      //Set resizeToAvoidBottomInset (101)
-      resizeToAvoidBottomInset: false,
+      body: AsymmetricView(products: ProductsRepository.loadProducts(Category.all)),
     );
   }
 }
